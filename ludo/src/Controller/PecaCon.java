0cc90d6b6fbdefa14918924f29d16049cd39d090
MICHEL\Michel Berigo <michelrayesberigo@hotmail.com>
2017-12-06 23:07:20 -0200

    Peças possuem cor. Quadrantes possuem id.

--- ludo/src/Controller/PecaCon.java
+++ ludo/src/Controller/PecaCon.java
@@ -1,6 +1,25 @@
 package Controller;
 
 public class PecaCon {
-	String cor;
 	int id;
+	
+	public enum Cor {
+	       AMARELO, AZUL, VERMELHO, VERDE;
+	}
+	
+	public static Cor getCorAmarelo() {
+		return Cor.AMARELO;
+	}
+	
+	public static Cor getCorAzul() {
+		return Cor.AZUL;
+	}
+	
+	public static Cor getCorVermelho() {
+		return Cor.VERMELHO;
+	}
+	
+	public static Cor getCorVERDE() {
+		return Cor.VERDE;
+	}
 }
--- ludo/src/Controller/QuadranteCon.java
+++ ludo/src/Controller/QuadranteCon.java
@@ -1,5 +1,7 @@
 package Controller;
 
 public class QuadranteCon {
-
+	int id;
+	
+	
 }
